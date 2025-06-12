e2a83ae6814ef503e31311dba65b4b95fd837dd8
sihwan <stay136@gmail.com>
2025-06-12 17:12:56 +0900

    edit:main.ts

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Kafka producer 연결
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'api-gateway',
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'api-gateway-consumer',
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
